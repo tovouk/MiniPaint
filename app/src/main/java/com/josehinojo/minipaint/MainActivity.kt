15dca81d9b9316a76cf9df626d2c21070d4d6aa6
Jose Hinojo <burningremedy@gmail.com>
2020-12-04 12:04:15 -0600

    set canvasview to main activity

--- app/src/main/java/com/josehinojo/minipaint/MainActivity.kt
+++ app/src/main/java/com/josehinojo/minipaint/MainActivity.kt
@@ -2,10 +2,19 @@ package com.josehinojo.minipaint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import androidx.core.view.doOnLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val myCanvasView = MyCanvasView(this)
+        // myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN deprecated
+        //below makes view fullscreen when it is on the layout
+        myCanvasView.doOnLayout {
+            window.setDecorFitsSystemWindows(false)
+        }
+        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+        setContentView(myCanvasView)
     }
 }
--- app/src/main/java/com/josehinojo/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/josehinojo/minipaint/MyCanvasView.kt
@@ -2,6 +2,7 @@ package com.josehinojo.minipaint
 
 import android.content.Context
 import android.view.View
+import android.view.Window
 
 class MyCanvasView(context: Context) : View(context){
 
